95fcca5b87dc005e4d8cc5b41621a25618040534
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2024-04-10 14:15:36 +0200

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.buy-button').addEventListener('click', function() {
-    alert('Thank you for your purchase!');
-    // Here you can add more functionality, like redirecting to a checkout page
-    // window.location.href = 'your-checkout-page-url';
-  });
-  
